dc34502f5e65cdbefdd52001035c90b0615cb437
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-06 14:20:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,4 +20,29 @@ client.on("guildMemberAdd", m => {
     };
 });
 
+const adminprefix = "$";
+const devs = ['439393453332234243','439393453332234243'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'بلاي')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'نيم')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'افتار')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'ستريم')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/medo149");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
+
 client.login(process.env.BOT_TOKEN);
