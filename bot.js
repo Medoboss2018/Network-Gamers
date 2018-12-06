08b0b528c5c1be9e3297cb03665cdc4508e021fe
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-06 14:11:48 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
+
+client.login(process.env.BOT_TOKEN);
