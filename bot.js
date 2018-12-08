ba1325618485c34b262d6b94f32348a52f99bf37
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-08 00:01:06 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -405,7 +405,7 @@ client.on('message', message => {
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`\`Support Team\` **لا توجد رتبة بأسم**`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`**لديك تذكرة مفتوحة بالفعل**`);
+    if (message.guild.channels.exists("name", "تذكرة-" + message.author.id)) return message.channel.send(`**لديك تذكرة مفتوحة بالفعل**`);
     message.guild.createChannel(`ticket`, "text").then(c => {
         let role = message.guild.roles.find("name", "Support Team");
         let role2 = message.guild.roles.find("name", "@everyone");
