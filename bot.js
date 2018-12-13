55519936df76cd06f04fae23802a42323c2ff9c2
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-13 10:17:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -606,30 +606,16 @@ if (message.content.startsWith(adminprefix + 'ستريم')) {
 }
 });
 
-const invites = {};
-
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-  wait(1000);
-
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "general");
-    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+    const stewart = member.guild.channels.find("name", "invited");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   });
-});
+})
 
 client.on('guildMemberAdd', member => {
     let channel = member.guild.channels.find('name', 'hello');
